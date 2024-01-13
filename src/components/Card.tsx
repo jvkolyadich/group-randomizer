f81273c929e399fd66961f5d658d9f66f7872448
James K <jvkolyadich@gmail.com>
2024-01-13 13:41:23 -0500

    added card padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ const Card = ({
   children
 }: Props) => {
   return (
-    <div className='w-11/12 md:w-3/4 max-w-[44rem] h-fit m-4 bg-white rounded p-4 flex flex-col items-center'>
+    <div className='w-11/12 md:w-3/4 max-w-[44rem] h-fit m-4 bg-white rounded p-4 flex flex-col items-center mb-6'>
       {children}
     </div>
   )
